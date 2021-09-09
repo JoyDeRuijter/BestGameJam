a736c4749bb7ba42b73ca72139f4e785662bacec
Selawen <37039183+Selawen@users.noreply.github.com>
2021-09-09 13:03:41 +0200

    last minute changes I didn't push

--- CHAOS/Assets/Scripts/ProjectileSpawner.cs
+++ CHAOS/Assets/Scripts/ProjectileSpawner.cs
@@ -11,29 +11,29 @@ public class ProjectileSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spawntime = Random.Range(40, 500);
+        spawntime = Random.Range(20, 300);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         timer++;
         if (timer == spawntime)
         {
             timer = 0;
-            spawntime = Random.Range(40, 500);
+            spawntime = Random.Range(20, 300);
             Spawn();
         }
     }
 
     void Spawn()
     {
-        GameObject laserInstance = Instantiate(laser, new Vector3(20, Random.Range(0, 10)),gameObject.GetComponent<Transform>().rotation);
+        GameObject laserInstance = Instantiate(laser, new Vector3(20, Random.Range(0, 5)),gameObject.GetComponent<Transform>().rotation);
 
         if (Random.Range(0,1.9f) > 1)
         {
             laserInstance.GetComponent<Laser>().speed.x *= -1;
-            laserInstance.transform.SetPositionAndRotation(new Vector3(-20, Random.Range(0, 10)), gameObject.GetComponent<Transform>().rotation);
+            laserInstance.transform.SetPositionAndRotation(new Vector3(-20, Random.Range(0, 5)), gameObject.GetComponent<Transform>().rotation);
             laserInstance.GetComponent<SpriteRenderer>().flipX = true;
         }
     }
